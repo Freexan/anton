dd8f16e86678f8ca97e9f17fe0e8ce866f274980
Freexan <chykaloalex@gmail.com>
2025-10-20 17:52:09 +0200

    feat: add CRUD routes

--- app/config/routes.php
+++ app/config/routes.php
@@ -1,6 +1,7 @@
-вщ<?php
+<?php
 
 use app\controllers\ApiExampleController;
+use app\controllers\ItemsController;
 use app\middlewares\SecurityHeadersMiddleware;
 use flight\Engine;
 use flight\net\Router;
@@ -10,9 +11,7 @@ use flight\net\Router;
  * @var Engine $app
  */
 
-// This wraps all routes in the group with the SecurityHeadersMiddleware
 $router->group('', function(Router $router) use ($app) {
-
 	$router->get('/', function() use ($app) {
 		$app->render('welcome', [ 'message' => 'You are gonna do great things!' ]);
 	});
@@ -21,10 +20,16 @@ $router->group('', function(Router $router) use ($app) {
 		echo '<h1>Hello world! Oh hey '.$name.'!</h1>';
 	});
 
-	$router->group('/api', function() use ($router) {
-		$router->get('/users', [ ApiExampleController::class, 'getUsers' ]);
-		$router->get('/users/@id:[0-9]', [ ApiExampleController::class, 'getUser' ]);
-		$router->post('/users/@id:[0-9]', [ ApiExampleController::class, 'updateUser' ]);
-	});
+	$router->get('/users', [ ApiExampleController::class, 'getUsers' ]);
+	$router->get('/users/@id:[0-9]+', [ ApiExampleController::class, 'getUser' ]);
+	$router->post('/users/@id:[0-9]+', [ ApiExampleController::class, 'updateUser' ]);
+
+	$router->get('/entities', [ ItemsController::class, 'index' ]);
+	$router->get('/entities/@id:[0-9]+', [ ItemsController::class, 'show' ]);
+	$router->post('/entities', [ ItemsController::class, 'create' ]);
+	$router->put('/entities/@id:[0-9]+', [ ItemsController::class, 'update' ]);
+	$router->delete('/entities/@id:[0-9]+', [ ItemsController::class, 'delete' ]);
+
+	$router->get('/items', [ ItemsController::class, 'page' ]);
 
 }, [ SecurityHeadersMiddleware::class ]);
